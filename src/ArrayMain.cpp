0c0b0695c23922e9bf8df5cae3813ae47bf9e3be
GoatKy1e <meekail.mugdho@gmail.com>
2025-04-04 16:31:35 +0800

    Array.ccp created with initial basic framework

--- src/ArrayMain.cpp
+++ src/ArrayMain.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
+class Functions{
+    //Sorting Fucntion Meekail
+    //Sortiung Function Jonathan yo
+    //Sortiung Fucntion Jonathan Harleson
+    //Sorting Fucntion Lim
+
+    //Searching Function Meekail
+    //Searching Fucntion 
+};
+
+class DataContainer2d{
+    
+
+};
+
+
 int main(){
     cout << "Hello World";
     return 0;
+
+    
 }
